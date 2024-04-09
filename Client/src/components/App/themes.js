b4c8d0d88a46d839cfc5175d5a87d32ba55fccdf
Anastasia Petrova <nastia_pet2017@mail.ru>
2024-04-10 00:16:17 +0300

    darkTheme, userInfo, Images added

---
+++ Client/src/components/App/themes.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components"
+
+export const lightTheme = {
+    body: "#fff",
+    text: "#363E45",
+    border: "1px solid rgb(52, 73, 94)"
+}
+
+export const darkTheme = {
+    body: "#212F3C",
+    text: "#fff",
+    border: "1px solid #fff"
+}
+
+export const GlobalStyles = createGlobalStyle`
+
+body, .profile {
+   background: ${(props) => props.theme.body};
+   color: ${(props) => props.theme.text};
+}
+
+.userPhoto {
+   border: ${(props) => props.theme.border};
+}
+
+.switchButton {
+    border: ${(props) => props.theme.border};
+}
+
+`
