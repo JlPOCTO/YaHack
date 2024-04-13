44adbd69d41fe62f91f7024c63ad6c8cc1e5d28b
klester <klesteralexey@yandex-team.ru>
2024-04-13 13:49:50 +0300

    vite build

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
 import {ThemeProvider} from "@gravity-ui/uikit";
 import Ro from "./Pages/RegisterPage/RegisterPage";
 import {BrowserRouter as Router} from 'react-router-dom';
@@ -24,8 +23,3 @@ root.render(
 
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
