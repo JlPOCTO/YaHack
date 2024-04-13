9d7959d7b1b59e71b6b887edc0f5529fb425c4ea
Ким Владимир Дмитриевич <kim_vd@ascon.ru>
2024-04-13 15:51:19 +0300

    added db to routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,7 +3,8 @@ const passport = require('passport');
 const {getBasePage} = require("./statics/getBasePage");
 const { isAuthenticatedMiddleware } = require("./middlewares/isAuthenticatedMiddleware")
 const dbChats = require('./database/dbChats');
-const dbUsers = require('./database/dbUsers');  
+const dbUsers = require('./database/dbUsers');
+const { db } = require('./database/db');
 
 const routers = express.Router();
 
