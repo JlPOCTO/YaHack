1345f84b5f940f66ba11597703b0261ee6fec4bd
Ким Владимир Дмитриевич <kim_vd@ascon.ru>
2024-04-13 14:21:35 +0300

    deleted debug code

--- server/src/createAllDBs.js
+++ server/src/createAllDBs.js
@@ -6,27 +6,4 @@
 
     await usersDB.createTables();
     await chatsDB.createTables();
-
-    const users = await sqlite.open({
-        filename: "./DB/Users.db",
-        driver: sqlite3.Database
-    });
-
-    const chats = await sqlite.open({
-        filename: "./DB/Chats.db",
-        driver: sqlite3.Database
-    });
-    await usersDB.addUser(users, 0, "Test testovich", "uyuigehbj");
-    await usersDB.addUser(users, 1, "Other one", "ss");
-    await usersDB.addUser(users, 2, "DROP DATABASE", "MessageDB");
-
-    await chatsDB.addChat(chats, [0, 1]);
-    await chatsDB.addChat(chats, [0, 2]);
-    await chatsDB.addChat(chats, [2, 1]);
-    await chatsDB.addMessage(chats, 0, 0, "아니요", new Date(2024, 4, 2, 12, 10).toISOString());
-    await chatsDB.addMessage(chats, 0, 1, "Here", new Date(2024, 4, 2, 12, 15).toISOString());
-    await chatsDB.addMessage(chats, 0, 1, "Мда", new Date(2024, 4, 2, 12, 20).toISOString());
-
-    await chatsDB.addMessage(chats, 1, 0, "아니요", new Date(2024, 1, 1, 12, 10).toISOString());
-    await chatsDB.addMessage(chats, 1, 2, "やめてください", new Date(2024, 4, 2, 12, 10).toISOString());
 })();
