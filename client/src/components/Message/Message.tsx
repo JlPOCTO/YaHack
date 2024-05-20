5f0afb7833eac0ee3ace003c229f4b166b8d740d
ElviraMinko <elvira.minko.03@bk.ru>
2024-05-20 21:32:51 +0300

    part of reactions

--- client/src/components/Message/Message.tsx
+++ client/src/components/Message/Message.tsx
@@ -1,17 +1,35 @@
 import React, {useState} from 'react';
 import '../../css/Message.css';
 import {useUserStore} from "../../stores/UserStore";
+import {Icon} from "@gravity-ui/uikit";
+import {FaceSmile} from "@gravity-ui/icons";
+import Picker, {EmojiClickData} from "emoji-picker-react";
+import Popup from "reactjs-popup";
+import { Heart } from 'lucide-react';
 
 type Message = {
     message: any;
+}
+const getInitialCurrentMessage = () => {
+    return sessionStorage.getItem('currentMessage') || '';
 }
 
 function Message(props: Message) {
     const {message} = props;
     const {userID} = useUserStore()
+
     function isMine() {
         return message.sender_id === userID
     }
+
+    const onEmojiClick = (curEmoji: EmojiClickData) => {
+        const currentMessage = sessionStorage.getItem('currentMessage')
+        const newMessage = currentMessage ? currentMessage + curEmoji.emoji : curEmoji.emoji
+        setCurrentMessage(newMessage)
+        sessionStorage.setItem('currentMessage', newMessage)
+    }
+    const [currrentMessage, setCurrentMessage] = useState(getInitialCurrentMessage())
+
     return (
         <>
             {!isMine() && <div className="container">
@@ -31,25 +49,45 @@ function Message(props: Message) {
                     </div>
                 </div>
             </div>}
-            {isMine() && <div className="myContainer">
-                <div className="arrow">
-                    <div className="outer"></div>
-                    <div className="inner"></div>
+            {isMine() && <div className="myReaction">
+                <div className="myContainer">
+                    <div className="notReaction">
+                        <div className="arrow">
+                            <div className="outer"></div>
+                            <div className="inner"></div>
+                        </div>
+                        <div className="message-body">
+                            <div className="author">
+                                <p>{message.sender_id}</p>
+                            </div>
+                            <div className="text">
+                                <p>{message.message}</p>
+                            </div>
+                            <div className="data">
+                                <p>{message.time}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="heart-button">
+                        <Popup
+                            trigger={
+                                <button className='currentSettingsMessage'>
+                                    <Icon className='Settings' data={FaceSmile}/>
+                                </button>}
+                            position="top left"
+                        >
+                            <div className='emojiPopup'>
+                                <Picker onEmojiClick={onEmojiClick}/>
+                            </div>
+                        </Popup>
+                    </div>
                 </div>
-                <div className="message-body">
-                    <div className="author">
-                        <p>{message.sender_id}</p>
-                    </div>
-                    <div className="text">
-                        <p>{message.message}</p>
-                    </div>
-                    <div className="data">
-                        <p>{message.time}</p>
-                    </div>
-                </div>
-            </div>}
+            </div>
+            }
+
         </>
-    );
+    )
+        ;
 }
 
 export default Message;
