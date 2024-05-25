3f9e454fee6aa24fed7ca2076bee92f1c33b6821
Anastasia Petrova <nastia_pet2017@mail.ru>
2024-05-25 15:54:20 +0300

    scrolling: good!

--- client/src/components/AddMessage/AddMessage.tsx
+++ client/src/components/AddMessage/AddMessage.tsx
@@ -14,7 +14,7 @@ const getInitialCurrentMessage = () => {
 }
 
 function AddMessage() {
-    const {dialogID, userID, setFlag, flag} = useUserStore()
+    let {dialogID, userID, setFlag, flag} = useUserStore()
     const ref = useRef<HTMLTextAreaElement>(null)
     const {t, i18n} = useTranslation();
     const [messages, setMessage] = useState([])
@@ -27,10 +27,8 @@ function AddMessage() {
         sessionStorage.setItem('currentMessage', newMessage)
     }
     const handleAddMessage = async () => {
-        console.log('flag1', flag)
-        let g = flag
-        setFlag(!g)
-        console.log('flag', flag)
+        const g = !flag
+        setFlag(g)
         const date = Date.now() + 10800000;
         const showTime = date
         if (currrentMessage !== "") {
