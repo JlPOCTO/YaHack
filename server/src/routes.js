569a686a1a2c3da48e5132072b20afe38d4e3350
Danil Gavrilov <gavrilovda.sh@gmail.com>
2024-05-25 13:54:57 +0300

    feat: в процессе написания рутов 3

--- server/src/routes.js
+++ server/src/routes.js
@@ -175,6 +175,77 @@ routers.delete(
     }
 )
 
+routers.get('/dialogs', async (req, res) => {
+    //currentUser to be added
+    res.send(await chats.getChatsByUser(1));
+});
+// routers.get(
+//     version + '/myInfo',
+//     (req, res) => {
+//         res.send(req.user);
+//     }
+// );
+routers.get('/me', async (req, res) => {
+    //currentUser to be added
+    // res.send(req.user);
+    res.send(await users.findUserByID(1));
+});
+routers.get('/contacts', async (req, res) => {
+    //currentUser to be added
+    res.send(await users.findAllUsers());
+});
+routers.get('/dialogs/:id/messages', async (req, res) => {
+    let a = req.query.id
+    res.send(await chats.getMessagesFromChat(a))
+});
+
+routers.get('/messages', async (req, res) => {
+    let a = req.query.id
+    res.send(await chats.getMessagesFromChat(a))
+});
+
+
+routers.post('/addMessage', async (req, res) => {
+    const message = req.body.message;
+    const chatID = req.body.chatID;
+    const senderID = req.body.senderID;
+    const time = req.body.time;
+    const imagePath = req.body.imagePath;
+    await chats.addMessage(chatID,senderID,message,time,imagePath)
+    res.send(await chats.getMessagesFromChat(chatID))
+    // console.log(await chats.getMessagesFromChat(chatID))
+})
+
+
+
+routers.get('/dialogs', async (req, res) => {
+    //currentUser to be added
+    res.send(await chats.getChatsByUser(1));
+});
+
+routers.get('/me', async (req, res) => {
+    //currentUser to be added
+    res.send(await users.findUserByID(1));
+});
+routers.get('/contacts', async (req, res) => {
+    //currentUser to be added
+    res.send(await users.findAllUsers());
+});
+routers.get('/dialogs/:id/messages', async (req, res) => {
+    let a = req.query.id
+    res.send(await chats.getMessagesFromChat(a))
+});
+
+routers.get('/messages', async (req, res) => {
+    let a = req.query.id
+    res.send(await chats.getMessagesFromChat(a))
+});
+
+// app.post('/addMessage', async(req, res) => {
+//     const dialogID = req.body.dialogID;
+//     chats.addMessage(req.body.message)
+// })
+
 
 // Рут по умолчанию (для неизвестных)
 routers.get(
