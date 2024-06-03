020005506668adbf96cda4770c54f15a5f7216ce
ElviraMinko <elvira.minko.03@bk.ru>
2024-06-03 22:09:34 +0300

    modal window for add grops

--- client/src/components/ProfileModalWindow/ProfileModalWindow.tsx
+++ client/src/components/ProfileModalWindow/ProfileModalWindow.tsx
@@ -1,42 +1,56 @@
 import '../../css/ProfileModalWindow.css';
-import { Modal } from '@gravity-ui/uikit';
+import {Modal} from '@gravity-ui/uikit';
 import Profile from "../Profile/Profile";
-import { useEffect, useState } from 'react'
-import { observer } from "mobx-react-lite";
-import { Sidebar } from 'primereact/sidebar';
+import {useEffect, useState} from 'react'
+import {observer} from "mobx-react-lite";
+import {Sidebar} from 'primereact/sidebar';
 
 type ProfileModalWindow = {
-  me: any;
+    me: any;
 }
 
 function ProfileModalWindow() {
-  const [visible, setVisible] = useState(false);
-  const [me, setMyInfo] = useState([])
-  useEffect(() => {
+    const [visible, setVisible] = useState(false);
+    const [me, setMyInfo] = useState([])
+    useEffect(() => {
 
-    const getMyInfo = async () => {
-      const res = await fetch('\me')
-      const me = await res.json();
-      setMyInfo(me)
-    }
-    getMyInfo()
-  }, [])
+        const getMyInfo = async () => {
+            const res = await fetch('\me')
+            const me = await res.json();
+            setMyInfo(me)
+        }
+        getMyInfo()
+    }, [])
 
-  return (
-    <div>
-      <button type='button' onClick={() => setVisible(true)} style={{ background: "none", border: "none" }} className="mr-2">
-        <svg className='Settings' width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 17V11" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" />
-          <circle cx="1" cy="1" r="1" transform="matrix(1 0 0 -1 11 9)" fill="#1C274C" />
-          <path d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" />
-        </svg>
-          </button>
-      <Sidebar visible={visible} onHide={() => setVisible(false)} style={{ background: 'white', padding: '15px 10px 15px 10px' }}>
-        <Profile me={me} />
-      </Sidebar>
-      {visible && <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', background: 'rgba(0, 0, 0, 0.5)', zIndex: 999 }}
-        onClick={() => setVisible(false)}></div>}
-    </div>
-  );
+    return (
+        <div>
+            <button type='button' onClick={() => setVisible(true)} style={{background: "none", border: "none"}}
+                    className="mr-2">
+                <svg className='Settings' width="800px" height="800px" viewBox="0 0 24 24" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 17V11" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                    <circle cx="1" cy="1" r="1" transform="matrix(1 0 0 -1 11 9)" fill="#1C274C"/>
+                    <path
+                        d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8"
+                        stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                </svg>
+            </button>
+            <Sidebar visible={visible} onHide={() => setVisible(false)}
+                     style={{background: '', padding: '15px 10px 15px 10px'}}>
+                <Profile me={me}/>
+            </Sidebar>
+            {visible && <div style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                background: 'rgba(0, 0, 0, 0.5)',
+                zIndex: 999
+            }}
+                             onClick={() => setVisible(false)}></div>}
+        </div>
+    );
 }
-export default observer (ProfileModalWindow);
+
+export default observer(ProfileModalWindow);
--- client/src/components/SideBarBody/SideBarBody.tsx
+++ client/src/components/SideBarBody/SideBarBody.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import '../../css/SideBarBody.css';
 // import '../../css/ChatBar.css';
 import ChatBar from "../ChatBar/ChatBar";
-import {Button} from "@gravity-ui/uikit";
+import {Button, Icon} from "@gravity-ui/uikit";
 import {useUserStore} from "../../stores/UserStore";
 import {observer} from "mobx-react-lite";
 import {action} from "mobx";
@@ -12,15 +12,22 @@ import SearchPersonBar from "../SearchPersonBar/SearchPersonBar";
 // import load = Simulate.load;
 import '../../i18n/config';
 import {useTranslation} from 'react-i18next';
+import {ArrowRotateRight, Pencil} from '@gravity-ui/icons';
+import Profile from "../Profile/Profile";
+import {Sidebar} from 'primereact/sidebar';
+import Contacts from "../Contacts/Contacts";
+import AddChat from "../AddChat/AddChat";
 
 const API_HOST = 'http://localhost:3000';
 
 
 function SideBarBody() {
     let dialog = {
-        id : 2,
-        time :1
+        id: 2,
+        time: 1
     }
+    const [contacts, setMyContacts] = useState([])
+    const [visible, setVisible] = useState(false);
     const {searchInput, setSearchInput} = useUserStore();
     const {t, i18n} = useTranslation();
     const [dialogs, setDialogs] = useState([])
@@ -34,6 +41,16 @@ function SideBarBody() {
 
     useEffect(() => {
 
+        const getMyInfo = async () => {
+            const res = await fetch('\contacts')
+            const contacts = await res.json();
+            setMyContacts(contacts)
+        }
+        getMyInfo()
+    }, [])
+
+    useEffect(() => {
+
         const getDialogs = async () => {
             const res = await fetch(`/dialogs`)
             const dialogs1 = await res.json()
@@ -42,6 +59,7 @@ function SideBarBody() {
 
         getDialogs()
     }, [])
+
 
     return (
         <>
@@ -53,6 +71,7 @@ function SideBarBody() {
                 {dialogs.map((dialog: any) =>
                     <ChatBar dialog={dialog}/>
                 )}
+                <AddChat/>
             </div>}
             {!isSearchInputEmpty() && <div style={{
                 overflowY: "auto",
